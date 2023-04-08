903509251c0aef25ee9eb20b3d21a45b1ed617fc
nerdymomocat <125716950+nerdymomocat@users.noreply.github.com>
2023-04-07 20:49:42 -0700

    Update module_color.js

--- scripts/module_color.js
+++ scripts/module_color.js
@@ -1,7 +1,7 @@
 const lowerLightnessThreshold = 0.5; // You can adjust this value to change the lower lightness constraint
 export const upperLightnessThreshold = 0.8; // You can adjust this value to change the upper lightness constraint
 
-const predefinedColors = ["#E9E5E3", "#FAEBDD", "#FBF3DB", "#DDEDEA", "#DDEBF1", "#EAE4F2", "#F4DFEB", "#FBE4E4"];
+export const predefinedColors = ["#E9E5E3", "#FAEBDD", "#FBF3DB", "#DDEDEA", "#DDEBF1", "#EAE4F2", "#F4DFEB", "#FBE4E4"];
 
 
 const isColorCloseToPredefinedColors = (color) => {
