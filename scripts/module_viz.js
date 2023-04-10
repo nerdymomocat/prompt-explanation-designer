eaa3f88c83ea03bfc044ad73af0277171f73e92e
nerdymomocat <125716950+nerdymomocat@users.noreply.github.com>
2023-04-09 18:59:15 -0700

    Update module_viz.js

--- scripts/module_viz.js
+++ scripts/module_viz.js
@@ -632,6 +632,8 @@ function downloadVisualization(svg) {
   canvas.width = outputWidth;
   canvas.height = outputHeight;
   const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#fffcf5";
+  ctx.fillRect(0, 0, outputWidth, totalHeight);
 
   img.onload = () => {
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
